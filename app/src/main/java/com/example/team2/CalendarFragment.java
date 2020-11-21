4914610981398b762184c229a0085174bd846a99
xiholliday <xavier.holliday@mavs.uta.edu>
2020-11-20 23:03:34 -0600

    The working menu with Tabs after LOGIN

--- app/src/main/java/com/example/team2/CalendarFragment.java
+++ app/src/main/java/com/example/team2/CalendarFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FacilityRentalsFragment#newInstance} factory method to
+ * Use the {@link CalendarFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FacilityRentalsFragment extends Fragment {
+public class CalendarFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class FacilityRentalsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FacilityRentalsFragment() {
+    public CalendarFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class FacilityRentalsFragment extends Fragment {
      * @return A new instance of fragment FacilityRentalsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static FacilityRentalsFragment newInstance(String param1, String param2) {
-        FacilityRentalsFragment fragment = new FacilityRentalsFragment();
+    public static CalendarFragment newInstance(String param1, String param2) {
+        CalendarFragment fragment = new CalendarFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class FacilityRentalsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_facility_rentals, container, false);
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
 }
--- app/src/main/java/com/example/team2/MainActivity.java
+++ app/src/main/java/com/example/team2/MainActivity.java
@@ -1,27 +1,87 @@
 package com.example.team2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.Logoutbutton).setOnClickListener(onclickListner);
 
-    if(FirebaseAuth.getInstance().getCurrentUser()==null){
-        startSignActivity();
+        if(FirebaseAuth.getInstance().getCurrentUser()==null)
+        {
+            startSignActivity();
+        }
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabBar);
+        TabItem tabTraining = findViewById(R.id.Trainings);
+        TabItem tabHealth = findViewById(R.id.Health);
+        TabItem tabChildren = findViewById(R.id.Children);
+        TabItem tabRental = findViewById(R.id.Calendar);
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        //this function changes the tabs view when the tab is selected
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_menu, menu);
+        return true;
+    }
 
+    public void openUrl(String url)
+    {
+        Uri uri = Uri.parse(url);
+        Intent launchWeb = new Intent(Intent.ACTION_VIEW);
+        launchWeb.setData(uri);
+        startActivity(launchWeb);
+    }
+    public void Sports_Training_Button(View view){
+        openUrl("https://docs.google.com/forms/d/e/1FAIpQLSfpPlcTnK2_YvMS_YeYQOE6YnSHabxRk-YaGPNDTT5nWFCxCw/viewform");
+    }
+    public void Basketball_Reg(View view){
+        openUrl("https://oneworldsa.leagueapps.com/leagues/basketball/1911049-lc-basketball-league");
+    }
+    public void Soccer_Reg(View view){
+        openUrl("https://oneworldsa.leagueapps.com/classes/1862566-soccer-sessions-tuesday-nights-nov---dec");
     }
 
     private void startSignActivity(){
@@ -29,23 +89,19 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    View.OnClickListener onclickListner = new View.OnClickListener() {
+    View.OnClickListener onclickListner = new View.OnClickListener()
+    {
         @Override
-        public void onClick(View v) {
-            switch (v.getId()){
+        public void onClick(View v)
+        {
+            switch (v.getId())
+            {
                 case R.id.Logoutbutton:
                     FirebaseAuth.getInstance().signOut();
                     startSignActivity();
                     break;
-
             }
         }
     };
 
-
-
-    private void sport(){
-        Intent intent = new Intent(this, SportsTrainingFragment.class);
-        startActivity(intent);
-    }
 }
