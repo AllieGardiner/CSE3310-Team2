5588511c9cd5129f8c4850879e06515c7e52f1f8
xiholliday <73303995+xiholliday@users.noreply.github.com>
2020-11-22 12:38:29 -0600

    Add files via upload

---
+++ CSE3310-Team2/app/src/main/java/com/example/team2/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.team2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.Logoutbutton).setOnClickListener(onclickListner);
+
+    if(FirebaseAuth.getInstance().getCurrentUser()==null){
+        startSignActivity();
+    }
+
+
+    }
+
+    private void startSignActivity(){
+        Intent intent = new Intent(this, SingupActivity.class);
+        startActivity(intent);
+    }
+
+    View.OnClickListener onclickListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.Logoutbutton:
+                    FirebaseAuth.getInstance().signOut();
+                    startSignActivity();
+                    break;
+
+            }
+        }
+    };
+
+
+
+    private void sport(){
+        Intent intent = new Intent(this, SportsTrainingFragment.class);
+        startActivity(intent);
+    }
+}
