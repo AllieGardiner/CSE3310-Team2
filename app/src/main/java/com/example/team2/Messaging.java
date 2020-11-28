32506a7bf7ae12c7d3d1ba4ee03cc940a63f4b9b
xiholliday <xavier.holliday@mavs.uta.edu>
2020-11-28 15:59:00 -0600

    Profile w/Working Message Action Button

---
+++ app/src/main/java/com/example/team2/Messaging.java
@@ -0,0 +1,14 @@
+package com.example.team2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Messaging extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+    }
+}
