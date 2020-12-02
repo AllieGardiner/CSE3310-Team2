7fa80dfba792e3bf213fb02f8a051b9f5932469a
AllieGardiner <71984059+AllieGardiner@users.noreply.github.com>
2020-12-01 20:12:44 -0600

    messaging update

---
+++ app/src/main/java/com/example/team2/Conversations.java
@@ -0,0 +1,92 @@
+package com.example.team2;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Conversations extends AppCompatActivity {
+
+    ListView lvConversations;
+    ArrayList<String> listOfConversation = new ArrayList<String>();
+    ArrayAdapter arrayAdpt;
+    String username;
+
+    private DatabaseReference dbr = FirebaseDatabase.getInstance().getReference().getRoot();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversations);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        lvConversations = (ListView) findViewById(R.id.conversationList);
+        arrayAdpt = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfConversation);
+        lvConversations.setAdapter(arrayAdpt);
+
+        dbr.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+
+                while(i.hasNext()){
+                    set.add(((DataSnapshot)i.next()).getKey());
+                }
+                arrayAdpt.clear();
+                arrayAdpt.addAll(set);
+                arrayAdpt.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
+
+
+        lvConversations.setOnItemClickListener((new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(), Messaging.class);
+                i.putExtra("selected_conversation", ((TextView)view).getText().toString());
+                startActivity(i);
+            }
+        }));
+
+        FloatingActionButton fab = findViewById(R.id.newConversationButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/team2/Messaging.java
+++ app/src/main/java/com/example/team2/Messaging.java
@@ -1,14 +1,117 @@
 package com.example.team2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Messaging extends AppCompatActivity {
+
+    private DatabaseReference dbr;
+    Button sendButton;
+    EditText editMsg;
+
+    ListView conversation;
+    ArrayList<String> listConversation = new ArrayList<String>();
+    ArrayAdapter arrayAdpt;
+
+    String username, SelectedConversation, user_msg_key;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        sendButton = (Button) findViewById(R.id.sendButton);
+        editMsg = (EditText) findViewById(R.id.editMessage);
+
+        conversation = (ListView) findViewById(R.id.lvMessaging);
+        arrayAdpt = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listConversation);
+        conversation.setAdapter(arrayAdpt);
+
+        username = getIntent().getExtras().get("user_name").toString();
+        SelectedConversation = getIntent().getExtras().get("selected_topic").toString();
+        setTitle(SelectedConversation);
+
+        dbr = FirebaseDatabase.getInstance().getReference().child(SelectedConversation);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> map = new HashMap<String, Object>();
+                user_msg_key = dbr.push().getKey();
+                dbr.updateChildren(map);
+
+                DatabaseReference dbr2 = dbr.child(user_msg_key);
+                Map<String, Object> map2 = new HashMap<String, Object>();
+                map2.put("msg", editMsg.getText().toString());
+                map2.put("user", username);
+                dbr2.updateChildren(map2);
+            }
+        });
+
+        dbr.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                updateConversation(snapshot);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                updateConversation(snapshot);
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
+
+    public void updateConversation(DataSnapshot snapshot){
+        String msg, user, conversation;
+        Iterator i = snapshot.getChildren().iterator();
+        while(i.hasNext()){
+            msg = (String) ((DataSnapshot)i.next()).getValue();
+            user = (String) ((DataSnapshot)i.next()).getValue();
+
+            conversation = user + ": " + msg;
+            arrayAdpt.insert(conversation, arrayAdpt.getCount());
+            arrayAdpt.notifyDataSetChanged();
+        }
+    }
+
+
 }
